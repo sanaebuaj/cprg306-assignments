3ff6dbb16c619e530bfe6ecb2ab6cbd190fcc99a
sanaebuaj <119626562+sanaebuaj@users.noreply.github.com>
2024-02-05 23:47:59 -0700

    my second commit

--- app/page.js
+++ app/page.js
@@ -3,11 +3,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center  p-24 ">
     <h1>CPRG 306: Web Development 2 - Assignments</h1>
-    <div>
+    <div className="p-5">
       <Link href={"week-2"} className="border border-black hover:bg-white">
         Click to go to week 2
+      </Link> 
+      <Link href={"week-3"} className="border border-black hover:bg-white">
+        Click to go to week 3
       </Link>
     </div>  
     
